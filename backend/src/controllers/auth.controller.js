c9e37cac25454ff9bd5341d598bae1e2ee1bfff5
Abhishek Shrestha <absk.shrestha@gmail.com>
2025-11-24 19:17:58 +0545

    Signup endpoint sans testing

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,9 +1,76 @@
-export async function signup(req, res){
-  res.send("Signup Route");
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+
+export async function signup(req, res) {
+  const { email, password, fullName } = req.body;
+
+  try {
+
+    if (!email || !password || !fullName) {
+      return res.status(400).json({ message: "All fiels are required" });
+    }
+
+    if (password.length < 6) {
+      return res.status(400).json({ message: "Passowod must be at least 6 characters" });
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already exists, pls use a diff one" });
+    }
+
+
+    const idx = Math.floor(Math.random() * 100) + 1; //generate a numbers between 1 and 100
+    const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
+
+    const newUser = new User.create({
+      email,
+      fullName,
+      password,
+      profilePic: randomAvatar
+    })
+
+
+    //TODO:CREATE THE USER IN STREAM AS WELL
+
+    const token = jwt.sign({ userId: newUser.id }, process.next.JWT_SECRET_KEY, { expiresIn: "7d" }
+    )
+
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true, //prevent XSS attacks
+      sameSite: "strict", //prevent CSRF attcks
+      secure: process.env.NODE_ENC === "production",
+    })
+
+    res.status(201).json({ succes: true, user: newUser })
+
+
+  } catch (error) {
+
+    console.log("Error in signup controller", error),
+      res.status(500).json({ message: "Internal Server Error" });
+
+  }
+
 }
-export async function login(req, res){
+
+
+
+
+
+
+
+export async function login(req, res) {
   res.send("Login Route");
 }
-export async function logout(req, res){
+export async function logout(req, res) {
   res.send("Logout Route");
 }
