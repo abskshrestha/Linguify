7551eb96957b9c38a5c1817c272a7d5186f63469
Abhishek Shrestha <absk.shrestha@gmail.com>
2025-11-26 22:41:58 +0545

    login and logout endpoint minus stream completed

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -49,28 +49,50 @@ export async function signup(req, res) {
       sameSite: "strict", //prevent CSRF attcks
       secure: process.env.NODE_ENV === "production",
     })
-
     res.status(201).json({ success: true, user: newUser })
-
-
   } catch (error) {
 
     console.log("Error in signup controller", error),
       res.status(500).json({ message: "Internal Server Error" });
-
   }
-
 }
-
-
-
-
 
 
 
 export async function login(req, res) {
-  res.send("Login Route");
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({message:"All fields are required"});
+    }
+
+    const user = await User.findOne({email});
+    if (!user) return res.status(401).json({message: "Invalid email or password"});
+
+    const isPasswordCorrect = await user.matchPassword(password);
+
+    if (!isPasswordCorrect) return res.status(401).json({message:"Invalid email or password"});
+
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET_KEY, { expiresIn: "7d",
+    });
+
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true, //prevent XSS attacks
+      sameSite: "strict", //prevent CSRF attcks
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    res.status(200).json({success:true, user});
+
+  } catch(error){
+    console.log("Error in login controller", error.message);
+    res.status(500).json({message: "Internal Server Error"});
+  }
 }
+
 export async function logout(req, res) {
-  res.send("Logout Route");
+  res.clearCookie("jwt")
+  res.status(200).json({success: true, message: "Logout succesful"});
 }
--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -49,12 +49,11 @@ const userSchema = new mongoose.Schema({
   ],
 }, { timestamps: true });
 
-// Fixed: Moved pre-save hook BEFORE model creation
+
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   
   try {
-    // Fixed: Changed bcrypt.gensalt to bcrypt.genSalt (capital S)
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
     next();
@@ -62,6 +61,17 @@ userSchema.pre("save", async function (next) {
     next(error);
   }
 });
+
+
+userSchema.methods.matchPassword = async function(enteredPassword){
+
+  const isPasswordCorrect = await bcrypt.compare(enteredPassword, this.password);
+  return isPasswordCorrect;
+
+}
+
+
+
 
 const User = mongoose.model("User", userSchema);
 
