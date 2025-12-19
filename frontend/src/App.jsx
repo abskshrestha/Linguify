938f897e455c115c0b65c7538f1d8f99da42dd1a
Abhishek Shrestha <absk.shrestha@gmail.com>
2025-12-19 14:13:39 +0545

    Tanstack Query setup completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,10 +8,33 @@ import CallPage from "./pages/CallPage";
 import ChatPage from "./pages/ChatPage";
 import OnboardingPage from "./pages/OnboardingPage";
 import toast, {Toaster} from "react-hot-toast";
+import { useEffect, useState } from "react";
+import {useQuery} from "@tanstack/react-query";
+import { axiosInstance } from "./lib/axios.js";
+
+
 
 const App = () => {
+  //tanstack query
 
-  
+  const {data, isLoading, error} = useQuery({
+    queryKey:"todos",
+
+    queryFn: async() => {
+      const res = await axiosInstance.get("/auth/me");
+      return res.data;
+    },
+    retry: false, //auth check
+  });
+
+  console.log(data);
+
+
+
+
+
+
+
 
   return <div className="h-screen" data-theme="night">
     <button onClick={()=> toast.success("Hello World!")}>Create a toast</button>
